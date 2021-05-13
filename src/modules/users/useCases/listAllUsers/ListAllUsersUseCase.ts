cbfe9aff86a4c24bcd0a8a5d4b03a4c35cc73235
Jonas Brito <jonasexplore@gmail.com>
2021-05-13 09:32:58 -0300

    finish challenge

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -9,7 +9,17 @@ class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user) {
+      throw new Error("USER_NOT_FOUND");
+    }
+
+    if (!user.admin) {
+      throw new Error("You must be an administrator user to list all users.");
+    }
+
+    return this.usersRepository.list();
   }
 }
 
